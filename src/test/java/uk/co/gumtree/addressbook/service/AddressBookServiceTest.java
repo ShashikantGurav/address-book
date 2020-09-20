8f403c21260fa42b9ef4b88b6169f575e44320a2
shashikantgurav <shashikant.gurav@gmail.com>
2020-09-20 15:57:04 +0100

    Test Added for numberOfPersonWithGender method

--- src/test/java/uk/co/gumtree/addressbook/service/AddressBookServiceTest.java
+++ src/test/java/uk/co/gumtree/addressbook/service/AddressBookServiceTest.java
@@ -1,6 +1,15 @@
 package uk.co.gumtree.addressbook.service;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import uk.co.gumtree.addressbook.domain.AddressBook;
+import uk.co.gumtree.addressbook.domain.Contact;
+import uk.co.gumtree.addressbook.domain.Gender;
+
+import java.time.LocalDate;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AddressBookServiceTest {
 
@@ -10,6 +19,21 @@ public class AddressBookServiceTest {
     @BeforeEach
     public void setup() {
         addressBookService = new AddressBookService();
+    }
+
+    @Test
+    public void testNumberOfPersonWithGender() {
+
+        AddressBook addressBook = AddressBook.builder()
+                .contactList(Collections.singletonList(Contact.builder()
+                        .dateOfBirth(LocalDate.MAX)
+                        .lastName("Smith")
+                        .firstName("Steve")
+                        .gender(Gender.MALE)
+                        .build()))
+                .build() ;
+        assertEquals(1 , addressBookService.numberOfPersonWithGender(addressBook, Gender.MALE));
+        assertEquals(0 , addressBookService.numberOfPersonWithGender(addressBook, Gender.FEMALE));
     }
 
 }
