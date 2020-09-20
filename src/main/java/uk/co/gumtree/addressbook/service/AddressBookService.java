6077556c5320c45a9b0dd4d7a563e3de9184ea7a
shashikantgurav <shashikant.gurav@gmail.com>
2020-09-20 17:01:53 +0100

    Read AddressBook code added.

--- src/main/java/uk/co/gumtree/addressbook/service/AddressBookService.java
+++ src/main/java/uk/co/gumtree/addressbook/service/AddressBookService.java
@@ -1,17 +1,30 @@
 package uk.co.gumtree.addressbook.service;
 
 import org.joda.time.Days;
+import org.joda.time.LocalDate;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 import uk.co.gumtree.addressbook.domain.AddressBook;
 import uk.co.gumtree.addressbook.domain.Contact;
 import uk.co.gumtree.addressbook.domain.Gender;
-import java.time.format.DateTimeFormatter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 public class AddressBookService {
 
-    private DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("d/MM/yyyy");
+    private final String SEPARATOR = ",";
+
+    private DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormat.forPattern("d/MM/yyyy");
 
     /**
      * Return number of person based on Gender
@@ -66,5 +79,80 @@ public class AddressBookService {
         int days = Days.daysBetween(firstPerson.getDateOfBirth(), otherPerson.getDateOfBirth()).getDays();
 
         return String.valueOf(days);
+    }
+
+    public AddressBook readAddressBookFromFile(String filePath) throws IOException {
+        AddressBook addressBook  = AddressBook.builder()
+                .build();
+
+        if (filePath == null) {
+            return addressBook;
+        }
+
+        InputStream pathResource = new ClassPathResource(filePath)
+                .getInputStream();
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(pathResource));
+
+        List<String> collect = bufferedReader.lines().collect(Collectors.toList());
+
+        collect.stream().map(this::parsePerson)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+
+        return addressBook;
+    }
+
+    public Contact parsePerson(String s) {
+
+        String[] personDetail = s.split(SEPARATOR);
+
+        if (personDetail == null || personDetail .length < 3 ) {
+            return null;
+        }
+
+        return Contact.builder()
+                .firstName(getFirstName(personDetail[0]))
+                .lastName(getLastName(personDetail[0]))
+                .dateOfBirth(parseDateOfBirth(personDetail[1]))
+                .gender(parseGender(personDetail[2]))
+                .build();
+    }
+
+    public Gender parseGender(String gender) {
+
+        if (gender == null){
+            return null;
+        }
+        return Gender.valueOf(gender.toUpperCase());
+    }
+
+    public LocalDate parseDateOfBirth(String date) {
+        if (date == null) {
+            return null;
+        }
+        return LocalDate.parse(date, DATE_TIME_FORMATTER);
+    }
+
+    public String getLastName(String name) {
+
+        String[] fullName = parseName(name);
+        if (fullName == null || fullName.length < 2) return null;
+        return fullName[1];
+    }
+
+    public String getFirstName(String name) {
+
+        String[] fullName = parseName(name);
+        if (fullName == null || fullName.length < 1) return null;
+        return fullName[0];
+    }
+
+
+    public String[] parseName(String name) {
+        if (name == null) {
+            return null;
+        }
+        String[] fullName = name.split(" ");
+        return fullName;
     }
 }
