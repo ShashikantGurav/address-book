84116d800b6bca5655116258572101b9cef80010
shashikantgurav <shashikant.gurav@gmail.com>
2020-09-20 16:17:21 +0100

    Code and Test Added for Q2

--- src/main/java/uk/co/gumtree/addressbook/service/AddressBookService.java
+++ src/main/java/uk/co/gumtree/addressbook/service/AddressBookService.java
@@ -2,20 +2,37 @@ package uk.co.gumtree.addressbook.service;
 
 import org.springframework.stereotype.Service;
 import uk.co.gumtree.addressbook.domain.AddressBook;
+import uk.co.gumtree.addressbook.domain.Contact;
 import uk.co.gumtree.addressbook.domain.Gender;
+
+import java.time.format.DateTimeFormatter;
+import java.util.Comparator;
 
 @Service
 public class AddressBookService {
+
+    private DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("d/MM/yyyy");
 
     /**
      * Return number of person based on Gender
      * @param addressBook
      * @param gender
-     * @return
+     * @return Long
      */
 
     public long numberOfPersonWithGender(AddressBook addressBook, Gender gender){
         return addressBook.getContactList().stream().filter(contact -> contact.getGender().equals(gender)).count();
     }
 
+    /**
+     * Return oldest person in address book
+     * @param addressBook
+     * @return Contact
+     */
+    public Contact getOldestPerson(AddressBook addressBook) {
+
+        Comparator<Contact> ageComparator = (p1, p2) -> p1.getDateOfBirth().compareTo(p2.getDateOfBirth());
+        return addressBook.getContactList().stream()
+                .min(ageComparator).get();
+    }
 }
--- src/test/java/uk/co/gumtree/addressbook/service/AddressBookServiceTest.java
+++ src/test/java/uk/co/gumtree/addressbook/service/AddressBookServiceTest.java
@@ -7,11 +7,15 @@ import uk.co.gumtree.addressbook.domain.Contact;
 import uk.co.gumtree.addressbook.domain.Gender;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AddressBookServiceTest {
+
+    private DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("d/MM/yyyy");
 
 
     private AddressBookService addressBookService;
@@ -34,6 +38,37 @@ public class AddressBookServiceTest {
                 .build() ;
         assertEquals(1 , addressBookService.numberOfPersonWithGender(addressBook, Gender.MALE));
         assertEquals(0 , addressBookService.numberOfPersonWithGender(addressBook, Gender.FEMALE));
+    }
+
+    @Test
+    public void testReturnOldestPerson() {
+        Contact steve = Contact.builder()
+                .dateOfBirth(LocalDate.parse("01/02/1987",DATE_TIME_FORMATTER))
+                .lastName("Smith")
+                .firstName("Steve")
+                .gender(Gender.MALE)
+                .build() ;
+
+        Contact john  = Contact.builder()
+                .dateOfBirth(LocalDate.parse("01/02/1990", DATE_TIME_FORMATTER))
+                .lastName("Smith")
+                .firstName("John")
+                .gender(Gender.MALE)
+                .build() ;
+
+        Contact bruce  = Contact.builder()
+                .dateOfBirth(LocalDate.parse("01/02/1985", DATE_TIME_FORMATTER))
+                .lastName("Smith")
+                .firstName("Bruce")
+                .gender(Gender.MALE)
+                .build() ;
+
+        AddressBook addressBook = AddressBook.builder()
+                .contactList(Arrays.asList(steve, john, bruce))
+                .build() ;
+
+        assertEquals(bruce ,addressBookService.getOldestPerson(addressBook));
+
     }
 
 }
